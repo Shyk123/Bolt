ed26054f0df0a7556e88348d563d86196d762e0f
Viktor42 <shyk54321@gmail.com>
2024-11-26 22:56:12 +0300

    +dorabotka

--- JustTest/app/src/main/java/com/example/justtest/MainActivity.kt
+++ JustTest/app/src/main/java/com/example/justtest/MainActivity.kt
@@ -75,7 +75,7 @@ class MainActivity : AppCompatActivity() {
           //  var slovaVstroke: List<String> = mutableListOf()
            // var textNew = contentNotes.split("\n")
           //  var striga = toString()
-            var slovaVstroke = arrayListOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+         //   var slovaVstroke = arrayListOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
            // val charArray2 = charArrayOf('H', 'e', 'l', 'l', 'o')
 
             contentNotes = deletWords(contentNotes, binding.editTextEng.text.toString(), binding.editTextRus.text.toString())
@@ -135,7 +135,8 @@ class MainActivity : AppCompatActivity() {
     fun deletWords(text: String, engWord: String, rusWord: String): String {
         // делим на элементы массива пришедшую строку
         var textNew = text.split("\n")
-        var slovaVstroke: Array<String> = arrayOf()
+       // var slovaVstroke = mutableListOf("")
+        var slovaVstroke  = mutableListOf<String>()
 
         var i = 0
         var i2 = 0
@@ -147,8 +148,12 @@ class MainActivity : AppCompatActivity() {
         var j3 = 0
 
 
-        var textNew2: CharArray = charArrayOf()
-        var textPriom: CharArray = charArrayOf('0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+            //   var textNew2: CharArray = mutableListOf<Char>()
+        // var textPriom: CharArray = charArrayOf('0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
+        var textNew2= mutableListOf<Char>()
+        var textPriom = mutableListOf<Char>()
+
+        var textTotal: String = ""
 
         var charsEng = engWord.toCharArray()
         var charsRus = rusWord.toCharArray()
@@ -157,41 +162,52 @@ class MainActivity : AppCompatActivity() {
         while (i < textNew.count()) {
 
             j = 2 // пропускаем '<' and '+,-,='
-            j2 = 0
+        //    j2 = 0
             n = 1
-            textNew2 = textNew[i].toCharArray() // строка для работы
+            textNew2 = textNew[i].toMutableList() // строка для работы
 
             while ( textNew2[j] != '/' ) {      // take eng. word
-                textPriom[j2] =  textNew2[j] // больше не вылетает, вылетает дальше
+                textPriom.add(textNew2[j])  // больше не вылетает, так как верно добавляю символ
                 j++
-                j2++
+             //   j2++
             }
+            j++
 
-            slovaVstroke[0]  = textPriom.concatToString() // превращаем в string
-            textPriom = charArrayOf() // обнуляем
+                // j2 = 0
 
-            j2 = 0
+                //slovaVstroke[0]  = textPriom.concatToString() // превращаем в string ------ А еще вылетает
+          //  slovaVstroke[0] = textPriom.joinToString("")
+           // slovaVstroke.add(toString(textPriom))
+            slovaVstroke.add(textPriom.joinToString(""))
+            textPriom = mutableListOf<Char>()// обнуляем
 
             while ( textNew2[j] != '>' ) {
                 if ( textNew2[j] != ',' ) {
-                    textPriom[j2] = textNew2[j]
+                    textPriom.add(textNew2[j])
                     j++
-                    j2++
+             //       j2++
                 }
                 else {
                     j++
-                    slovaVstroke[n]  = textPriom.concatToString()
-                    textPriom = charArrayOf() // обнуляем
-                    j2 = 0
+                  //  slovaVstroke[n] = textPriom.joinToString("")  // было - textPriom.concatToString()
+                    slovaVstroke.add(textPriom.joinToString(""))
+                    textPriom = mutableListOf<Char>() // обнуляем
+             //       j2 = 0
                     n++
                 }
             }
+           // slovaVstroke[n] = textPriom.joinToString("") // вылет
+            slovaVstroke.add(textPriom.joinToString(""))
 
-            textPriom = slovaVstroke[0].toCharArray()
-            j2 = textPriom.size
+            j2 = 0
+        //    textPriom = slovaVstroke[0].toMutableList()
+
 // --------------------- основная проверка
             g = 0
             poz = 0
+            textPriom = mutableListOf<Char>()
+            textPriom.addAll(slovaVstroke[0].toList())
+            j2 = textPriom.size
 
             if ((j2 == engWord.toCharArray().size) || (j2 + 1 == engWord.toCharArray().size) || (j2 - 1 == engWord.toCharArray().size)) {
 
@@ -201,6 +217,7 @@ class MainActivity : AppCompatActivity() {
                     if ((charsEng[0] == textPriom[1]) && (charsEng[1] == textPriom[2])) {
                         poz = 1
                         g = 1
+                        j = 1
                     }
                     if ((charsEng[1] == textPriom[0]) && (charsEng[2] == textPriom[1])) {
                         poz = -1
@@ -208,6 +225,9 @@ class MainActivity : AppCompatActivity() {
                         j = 1
                     }
                 }
+            }
+            else{
+                g = 3
             }
 
                 while ((j < j2)&&(g!=2))	   {
@@ -218,12 +238,14 @@ class MainActivity : AppCompatActivity() {
                     }
                     j++
                 }   // типо проверили
+                poz = 0
 
                 if ( g < 2 ) {	// ----- попадаем сюда если в слове менее двух ошибок
                     j3=1
                     while ( j3<n+1 ) {
-                        textPriom = charArrayOf() 	// обнуляем
-                        textPriom = slovaVstroke[j].toCharArray()
+                        textPriom = mutableListOf<Char>()	// обнуляем
+                            //  textPriom = slovaVstroke[j].toMutableList() // вылет
+                        textPriom.addAll(slovaVstroke[j3].toList())
                         j2 = textPriom.size
                         g = 0
                         //--------------и тут типо основная проверка «два»------------------------=======================================
@@ -243,27 +265,42 @@ class MainActivity : AppCompatActivity() {
                                 }
                             }
                         }
+                        else {
+                            g = 3
+                        }
 
                         while ((j < j2)&&(g!=2))	   {
-                            if (charsRus[j] == textPriom[j+poz])  {
+                            if (charsRus[j] == textPriom[j+poz])  {    // че то тут не так!
                             }
                             else {
                                 g++
                             }
+                            poz = 0
 
                             if (g<2) {
+                                g = 0
+                                while (g < textNew.count()) {
+                                    if ( g != i ) {
+                                        textTotal += textNew[g]
+                                        textTotal += '\n'
+                                    }
+                                    g++
+                                }
                                 Toast.makeText(this, "Юхухуху", Toast.LENGTH_SHORT).show()
+                                return textTotal
                             }
 
                             j++
                         }   // типо проверили
-                        //=========================================================================================================
+                        //============================================================================================
                         j3++
                     }
                 }
+                slovaVstroke  = mutableListOf<String>()
+                textPriom = mutableListOf<Char>()
                 i++
             }
-            return textNew[0]
+        return text
         }
 
     } // ======================= скоба края ===============================
