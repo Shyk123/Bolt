50eee4d0a105853233d2d32b5ddaeaadd9c27faf
Normadia <shyk54321@gmail.com>
2024-11-28 17:39:06 +0300

    +++deletWord fun

--- JustTest/app/src/main/java/com/example/justtest/MainActivity.kt
+++ JustTest/app/src/main/java/com/example/justtest/MainActivity.kt
@@ -32,20 +32,23 @@ class MainActivity : AppCompatActivity() {
         val fileNameAssets = "notes505.txt"
         val fileNameStorage = "notes5.txt"
         var contentNotes = readFromInternalStorage(this, fileNameStorage)
+        var contentNotesNew = ""
 
         // ---Проверяем наличие словаря во внутреннем хранилище, в случае отстуствия---
         binding.buttonCheck.setOnClickListener {
             if (!isFileExistsInInternalStorage(this, fileNameStorage)) {
                 // ... создаём файл скопировав первичный словарь из файла в папки ассетс---
-        if (createFileInInternalStorage(this, fileNameStorage,readFileFromAssets(this, fileNameAssets)))
-        {
-            Toast.makeText(this, "Первый заход, словарь создан", Toast.LENGTH_SHORT).show()
-        }
-        else {
-             Toast.makeText(this, "Что-то пошло не так...", Toast.LENGTH_SHORT).show()
-             }
-        }
-            else{
+                if (createFileInInternalStorage(
+                        this,
+                        fileNameStorage,
+                        readFileFromAssets(this, fileNameAssets)
+                    )
+                ) {
+                    Toast.makeText(this, "Первый заход, словарь создан", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Что-то пошло не так...", Toast.LENGTH_SHORT).show()
+                }
+            } else {
                 Toast.makeText(this, "Вижу словарь", Toast.LENGTH_SHORT).show()
             }
         }
@@ -54,8 +57,7 @@ class MainActivity : AppCompatActivity() {
             if (isFileExistsInInternalStorage(this, fileNameStorage)) {
                 this.deleteFile(fileNameStorage)
                 Toast.makeText(this, "Словарь удален", Toast.LENGTH_SHORT).show()
-            }
-            else{
+            } else {
                 Toast.makeText(this, "Словаря и так нет", Toast.LENGTH_SHORT).show()
             }
         }
@@ -64,27 +66,38 @@ class MainActivity : AppCompatActivity() {
             if (isFileExistsInInternalStorage(this, fileNameStorage)) {
                 contentNotes = readFromInternalStorage(this, fileNameStorage)
                 binding.textWorld.text = contentNotes
-            }
-            else {
+            } else {
                 Toast.makeText(this, "Нечего выводить(", Toast.LENGTH_SHORT).show()
             }
         }
 
         binding.buttonDelWord.setOnClickListener {
-            //  contentNotes = deletWords(contentNotes, binding.editTextEng.text.toString(), binding.editTextRus.text.toString())
-          //  var slovaVstroke: List<String> = mutableListOf()
-           // var textNew = contentNotes.split("\n")
-          //  var striga = toString()
-         //   var slovaVstroke = arrayListOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
-           // val charArray2 = charArrayOf('H', 'e', 'l', 'l', 'o')
 
-            contentNotes = deletWords(contentNotes, binding.editTextEng.text.toString(), binding.editTextRus.text.toString())
-            binding.textWorld.text = contentNotes
+            if (binding.editTextEng.text.toString() != "") {
+                if (binding.editTextRus.text.toString() != "") {
+
+                    contentNotesNew = deletWords(
+                        contentNotes,
+                        binding.editTextEng.text.toString(),
+                        binding.editTextRus.text.toString()
+                    )
+                    if (contentNotes != contentNotesNew) {
+                        binding.textWorld.text = contentNotesNew
+                        contentNotes = contentNotesNew
+                        contentNotesNew = ""
+                        createFileInInternalStorage(this, fileNameStorage, contentNotes)
+                    } else {
+                        binding.textWorld.text = "нету такого сочитания"
+                    }
+                } else {
+                    binding.textWorld.text = "не ввел русское слово"
+                }
+            } else {
+                binding.textWorld.text = "не ввел англиское слово"
+            }
         }
 
-
-
-    }
+    }//========================    Последняя скоба для метода onCreat  ==========================
 
     fun isFileExistsInInternalStorage(context: Context, fileName: String): Boolean {
         val file = File(context.filesDir, fileName)
@@ -129,90 +142,70 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-//  while (j < textNew[i].toCharArray().size) {
-
-
     fun deletWords(text: String, engWord: String, rusWord: String): String {
-        // делим на элементы массива пришедшую строку
+        // получаем элементы массива из пришедшей строки
         var textNew = text.split("\n")
-       // var slovaVstroke = mutableListOf("")
-        var slovaVstroke  = mutableListOf<String>()
 
+        var slovaVstroke  = mutableListOf<String>()
         var i = 0
-        var i2 = 0
         var j = 0
         var j2 = 0
         var n = 1
         var g = 0
+        var g2 = 0
         var poz = 0
         var j3 = 0
-
-
-            //   var textNew2: CharArray = mutableListOf<Char>()
-        // var textPriom: CharArray = charArrayOf('0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0')
         var textNew2= mutableListOf<Char>()
         var textPriom = mutableListOf<Char>()
-
         var textTotal: String = ""
 
         var charsEng = engWord.toCharArray()
         var charsRus = rusWord.toCharArray()
 
-        // гуляем по циклу пока есть элементы в массиве
-        while (i < textNew.count()) {
+        // гуляем по циклу пока есть элементы в массиве или мы не найдем искомое
+        while (i < textNew.count()-1) {        // последний элемент всегда пустой
 
-            j = 2 // пропускаем '<' and '+,-,='
-        //    j2 = 0
-            n = 1
+            j = 2 // пропускаем '<' and '+/-/='
+            n = 1 // кол-во слов в "массиве текущего элемента"
             textNew2 = textNew[i].toMutableList() // строка для работы
 
-            while ( textNew2[j] != '/' ) {      // take eng. word
-                textPriom.add(textNew2[j])  // больше не вылетает, так как верно добавляю символ
+            while (textNew2[j] != '/') {      // take eng. word
+                textPriom.add(textNew2[j])  // посимвольно собираем слово
                 j++
-             //   j2++
             }
-            j++
+            j++ // пропускаем слэшь
 
-                // j2 = 0
-
-                //slovaVstroke[0]  = textPriom.concatToString() // превращаем в string ------ А еще вылетает
-          //  slovaVstroke[0] = textPriom.joinToString("")
-           // slovaVstroke.add(toString(textPriom))
-            slovaVstroke.add(textPriom.joinToString(""))
+            slovaVstroke.add(textPriom.joinToString("")) // английское слово записали
             textPriom = mutableListOf<Char>()// обнуляем
 
-            while ( textNew2[j] != '>' ) {
-                if ( textNew2[j] != ',' ) {
-                    textPriom.add(textNew2[j])
-                    j++
-             //       j2++
+            while (textNew2[j] != '>') {                // добываем перевод
+                if (textNew2[j] != ' ') {               // на всякий случай игнорим пробелы
+                    if (textNew2[j] != ',') {           // может быть несколько переводов одного слова
+                        textPriom.add(textNew2[j])      // посимвольно собираем перевод слова
+                        j++
+                    } else {
+                        j++
+                        slovaVstroke.add(textPriom.joinToString(""))
+                        textPriom = mutableListOf<Char>() // обнуляем
+                        n++
+                    }
                 }
-                else {
-                    j++
-                  //  slovaVstroke[n] = textPriom.joinToString("")  // было - textPriom.concatToString()
-                    slovaVstroke.add(textPriom.joinToString(""))
-                    textPriom = mutableListOf<Char>() // обнуляем
-             //       j2 = 0
-                    n++
-                }
+                else { j++ }
             }
-           // slovaVstroke[n] = textPriom.joinToString("") // вылет
-            slovaVstroke.add(textPriom.joinToString(""))
+            slovaVstroke.add(textPriom.joinToString("")) // получаем перевод
 
-            j2 = 0
-        //    textPriom = slovaVstroke[0].toMutableList()
+// --------------------- основная проверка ------- самих слов друг с другом ------------------------
 
-// --------------------- основная проверка
-            g = 0
             poz = 0
             textPriom = mutableListOf<Char>()
             textPriom.addAll(slovaVstroke[0].toList())
             j2 = textPriom.size
+            j = 0
+            g = 0
 
             if ((j2 == engWord.toCharArray().size) || (j2 + 1 == engWord.toCharArray().size) || (j2 - 1 == engWord.toCharArray().size)) {
 
-                j = 0
-                if (charsEng[0] != textPriom[0]) {        // проверяем первую букву, если нет то на позицию
+                if (j2 != engWord.toCharArray().size) {        // проверяем размер, если нет то на позицию
 
                     if ((charsEng[0] == textPriom[1]) && (charsEng[1] == textPriom[2])) {
                         poz = 1
@@ -225,81 +218,90 @@ class MainActivity : AppCompatActivity() {
                         j = 1
                     }
                 }
-            }
-            else{
+            } else {
                 g = 3
             }
 
-                while ((j < j2)&&(g!=2))	   {
-                    if (charsEng[j] == textPriom[j+poz])  {
-                        }
-                    else {
-                        g++
+            while ((j < (j2-1)) && (g < 2)) {
+                if (charsEng[j] == textPriom[j + poz]) {
+                } else {
+                    if (charsEng[j] == textPriom[j + 1]) {
+                        poz = 1
                     }
-                    j++
-                }   // типо проверили
-                poz = 0
+                    g++
+                }
+                j++
+            }   // типо проверили
+            poz = 0
 
-                if ( g < 2 ) {	// ----- попадаем сюда если в слове менее двух ошибок
-                    j3=1
-                    while ( j3<n+1 ) {
-                        textPriom = mutableListOf<Char>()	// обнуляем
-                            //  textPriom = slovaVstroke[j].toMutableList() // вылет
-                        textPriom.addAll(slovaVstroke[j3].toList())
-                        j2 = textPriom.size
-                        g = 0
-                        //--------------и тут типо основная проверка «два»------------------------=======================================
-                        if ((j2 == rusWord.toCharArray().size) || (j2 + 1 == rusWord.toCharArray().size) || (j2 - 1 == rusWord.toCharArray().size)) {
+            if (g < 2) {    // ----- попадаем сюда если в слове менее двух ошибок
+                j3 = 1
+                while (j3 < n + 1) {
+                    textPriom = mutableListOf<Char>()    // обнуляем
+                    //  textPriom = slovaVstroke[j].toMutableList() // вылет
+                    textPriom.addAll(slovaVstroke[j3].toList())
+                    j2 = textPriom.size
+                    g2 = 0
+                    j = 0
+                    //--------------и тут типо основная проверка «два»------------------------=======================================
+                    if ((j2 == rusWord.toCharArray().size) || (j2 + 1 == rusWord.toCharArray().size) || (j2 - 1 == rusWord.toCharArray().size)) {
 
-                            j = 0
-                            if (charsRus[0] != textPriom[0]) {        // проверяем первую букву, если нет то на позицию
+                        if (j2 != rusWord.toCharArray().size) {        // проверяем размер, если нет то на позицию
 
-                                if ((charsRus[0] == textPriom[1]) && (charsRus[1] == textPriom[2])) {
-                                    poz = 1
-                                    g = 1
-                                }
-                                if ((charsRus[1] == textPriom[0]) && (charsRus[2] == textPriom[1])) {
-                                    poz = -1
-                                    g = 1
-                                    j = 1
-                                }
+                            if ((charsRus[0] == textPriom[1]) && (charsRus[1] == textPriom[2])) {
+                                poz = 1
+                                g2 = 1
+                            }
+                            if ((charsRus[1] == textPriom[0]) && (charsRus[2] == textPriom[1])) {
+                                poz = -1
+                                g2 = 1
+                                j = 1
                             }
                         }
-                        else {
-                            g = 3
+                    } else {
+                        g2 = 3
+                    }
+
+                    while ((j < (j2-1)) && (g2 < 2)) {                  // для Рус нужно еще обработать: 'n' слов
+                        if (charsRus[j] == textPriom[j + poz]) {
+                        } else {
+                            if (charsRus[j] == textPriom[j + 1]) {
+                                poz = 1
+                            }
+                            g++
                         }
+                        j++
+                    }
 
-                        while ((j < j2)&&(g!=2))	   {
-                            if (charsRus[j] == textPriom[j+poz])  {    // че то тут не так!
-                            }
-                            else {
-                                g++
-                            }
-                            poz = 0
-
-                            if (g<2) {
-                                g = 0
-                                while (g < textNew.count()) {
-                                    if ( g != i ) {
-                                        textTotal += textNew[g]
+                    if ((g2 < 2 ) && (g < 2 )  ) {
+                        j2 = 0
+                        while (j2 < textNew.count()) {
+                            if (textNew[j2].toCharArray().size > 1) {
+                                if (j2 != i) {
+                                    textTotal += textNew[j2]
+                                    if (j2 + 1 != textNew.count()) {
                                         textTotal += '\n'
                                     }
-                                    g++
                                 }
-                                Toast.makeText(this, "Юхухуху", Toast.LENGTH_SHORT).show()
-                                return textTotal
                             }
-
-                            j++
-                        }   // типо проверили
-                        //============================================================================================
-                        j3++
+                            j2++
+                        }
+                        if ((g == 1) || (g2 == 1)) {
+                            Toast.makeText(this, "Ц, почти: " +slovaVstroke[0] +" " +slovaVstroke[n], Toast.LENGTH_SHORT).show()
+                        }
+                        else {
+                            Toast.makeText(this, "Юху - все четко", Toast.LENGTH_SHORT).show()
+                        }
+                        return textTotal
                     }
-                }
-                slovaVstroke  = mutableListOf<String>()
-                textPriom = mutableListOf<Char>()
-                i++
+
+                }   // типо проверили
+                //============================================================================================
             }
+            slovaVstroke = mutableListOf<String>()
+            textPriom = mutableListOf<Char>()
+            i++
+        }
         return text
         }
 
